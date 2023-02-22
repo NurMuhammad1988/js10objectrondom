f93dfc1be3b33aad5ccb01a1ec3e4efae1dfc190
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-02-23 01:22:13 +0500

    rondom

---
+++ app.js
@@ -0,0 +1,133 @@
+
+
+
+// var user = {
+//     Id:111,
+//     Name:'Jamshid',
+//     Surname: 'Ali',
+//     Age:23,
+//     IsAdmin:'True',
+
+//     userfunc : function(){
+//         alert(this.Name + this.Surname)
+//     }
+
+// }
+
+// var user2 = {
+//     Id:111,
+//     Name:'Jamshid2',
+//     Surname: 'Ali2',
+//     Age:23,
+//     IsAdmin:'True',
+
+//     userfunc : function(){
+//         alert(this.Name + this.Surname)
+//     }
+
+// }
+
+// var user3 = {
+//     Id:111,
+//     Name:'Jamshid3',
+//     Surname: 'Ali3',
+//     Age:23,
+//     IsAdmin:'True',
+
+//     userfunc : function(){
+//         alert(this.Name + this.Surname)
+//     }
+
+// }
+
+// user.userfunc ();
+// user2.userfunc ();
+// user3.userfunc ();
+
+// console.log(user);
+
+// for (var key in user){
+//     document.write(key + ':' + user [key] + '<br>' )
+// }
+
+
+
+
+
+
+
+
+// var rand1 =  Math.floor(Math.random() * 10 );
+// document.write(rand1)
+
+// var rand2 =  Math.ceil(Math.random() * 10 );
+// document.write(rand2)
+
+// var rand3 =  Math.round(Math.random() * 10 );
+// document.write(rand3)
+
+// var rand4 =  Math.trunc(Math.random() * 10 );
+// document.write(rand4)
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// delete user.isAdmin;
+// delete user.age;
+//  user.isAdmin = 'false';
+
+
+
+
+// for (var i = 1; i < user; i++){
+//     document.write(key + '<br>')
+// } hato
+
+
+
+// function makeUser(name, age) {
+//     return {
+//       name: name,
+//       age: age,
+//       // ...other properties
+//     };
+//   }
+  
+//   let user = makeUser("John 30", 30);
+//   alert(user.name); // John
